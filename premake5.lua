c31266c8a12c07841e1dc4dc8165cee7b3434eeb
Mayush Kumar <mayushkumar@pop-os.localdomain>
2020-05-08 22:50:36 +0530

    Added: Linux Support

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-project "GLFW"
+project "Glad"
 	kind "StaticLib"
 	language "C"
 	staticruntime "on"
@@ -8,39 +8,22 @@ project "GLFW"
 
 	files
 	{
-		"include/glfw/glfw3.h",
-		"include/glfw/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+		"include/glad/glad.h",
+		"include/KHR/khrplatform.h",
+		"src/glad.c"
+	}
+
+	includedirs
+	{
+		"include"
 	}
 
 	filter "system:windows"
 		--buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
 
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_thread.c",
-			"src/win32_time.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines
-		{
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
+	filter "system:linux"
+		toolset "gcc"
 
 	filter "configurations:Debug"
 			runtime "Debug"
