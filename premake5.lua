9be208ddf24dbe4e4975dd355c5657eb515b0a57
MayushKumar <mayushkumar2004@gmail.com>
2020-05-09 23:11:59 +0530

    Added: Linux Support

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-project "Glad"
+project "GLFW"
 	kind "StaticLib"
 	language "C"
 	staticruntime "on"
@@ -8,22 +8,62 @@ project "Glad"
 
 	files
 	{
-		"include/glad/glad.h",
-		"include/KHR/khrplatform.h",
-		"src/glad.c"
-	}
-
-	includedirs
-	{
-		"include"
+		"include/glfw/glfw3.h",
+		"include/glfw/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+		"src/vulkan.c",
+		"src/window.c"
 	}
 
 	filter "system:windows"
 		--buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
 
+		files
+		{
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_thread.c",
+			"src/win32_time.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
+
+		defines
+		{
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
 	filter "system:linux"
-		toolset "gcc"
+		toolset "gcc"	
+		pic "On"
+
+		files
+		{
+			"src/x11_init.c",
+			"src/linux_joystick.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
 
 	filter "configurations:Debug"
 			runtime "Debug"
